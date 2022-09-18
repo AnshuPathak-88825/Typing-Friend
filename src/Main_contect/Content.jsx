a0e45e54c60fa8e7714fb332fee0118dd9d16452
kamalmurmu1 <kamalmurmu420@gmail.com>
2022-09-18 20:11:56 +0530

    Added new files and modified

--- src/Main_contect/Content.jsx
+++ src/Main_contect/Content.jsx
@@ -1,4 +1,11 @@
+import "./Content.css";
+
 const Content = () => {
-  return <section id="content">Content</section>;
+  return (
+  <section id="content">
+    <textarea name="input_text1" id="input_text1_id" cols="100" rows="40" placeholder="Enter your text"></textarea>
+    <input type="submit" value="Clear" />
+  </section>
+  );
 };
 export default Content;

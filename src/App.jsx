71ffa286263ca9e14a02cb87614373d55c7cab0b
kamalmurmu1 <kamalmurmu420@gmail.com>
2022-09-18 19:08:23 +0530

    Added css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Content from "./Main_contect/Content";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import "./App.css";
+
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Header />
       <Content />
       <Footer/>
-    </>
+    </div>
   );
 };
 export default App;
